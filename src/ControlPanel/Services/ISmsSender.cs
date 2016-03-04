69373c7184f9d70a28adfe4b403c3b747bc14fa1
Adam Wyss <wyssa@hotmail.com>
2016-03-04 09:34:45 -0800

    Asp.Net MVC 6 default visual studio template; updated copyright snippet.

---
+++ src/ControlPanel/Services/ISmsSender.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ControlPanel.Services
+{
+    public interface ISmsSender
+    {
+        Task SendSmsAsync(string number, string message);
+    }
+}
